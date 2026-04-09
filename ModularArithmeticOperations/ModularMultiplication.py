6751bec559177ae7b9df1bf3f385e523f4a108a3
Flyli0 <kirillrozkov653@gmail.com>
2026-04-09 22:30:50 +0500

    Commented Modular multiplication

--- ModularArithmeticOperations/ModularMultiplication.py
+++ ModularArithmeticOperations/ModularMultiplication.py
@@ -1,9 +1,9 @@
-def square_and_multiply(base,exponent,modulus):
-    result = 1
-    base = base % modulus
-    while exponent>0:
-        if exponent%2!=0:
-            result = (result * base)%modulus
-        exponent = exponent >> 1
-        base = (base * base)%modulus
+def square_and_multiply(base,exponent,modulus):  # fun fact the pow() function is C realization of this algorithm
+    result = 1  # result is 1 because it does not affect other multiplications
+    base = base % modulus  # initial values
+    while exponent>0:  # this condition because we will right shift exponent until it becomes 0
+        if exponent%2!=0:  # it shows that lower bit is 1
+            result = (result * base)%modulus  # if our result has reminder it means that our rightmost bit = 1
+        exponent = exponent >> 1  # that is rightshift (We've could write just exponent//=exponent thou) выпендрёж
+        base = (base * base)%modulus  # raise it to the power of two and give it to the next person
     return result
