a82d59a3d6461f9d4259bf119638f0c4acedffdd
ibrayeviskander <iska2527@gmail.com>
2026-04-10 14:23:27 +0500

    Implemented Extended Euclidean Algorithm

---
+++ ModularArithmeticOperations/ExtendedEuclideanAlgorithm.py
@@ -0,0 +1,14 @@
+def inverse(a, m):
+    i = m
+    j = a
+    y2 = 0
+    y1 = 1
+    while j > 0:
+        quotient = i // j
+        remainder = i - j * quotient
+        y = y2 - y1 * quotient
+        i = j
+        j = remainder
+        y2 = y1
+        y1 = y
+    return y2 % m
