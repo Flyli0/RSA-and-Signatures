a61687f5b44978c92043166a80dc2d780fe33d59
ibrayeviskander <iska2527@gmail.com>
2026-04-10 13:42:24 +0500

    Worked on key generator

--- RSA_Key_Generation/KeysGenerator.py
+++ RSA_Key_Generation/KeysGenerator.py
@@ -1,8 +1,15 @@
 from PrimeNumberGeneration.PrimeGenerator import generate_prime
+from ModularArithmeticOperations.GCD import gcd
 
 def generate_keys(bit_size):
-    while (True):
+    while True:
         p = generate_prime(bit_size / 2)
         q = generate_prime(bit_size / 2)
-        if (abs(p - q) > 2 ** (bit_size / 2 - 100)):
-            
+        if abs(p - q) > 2 ** (bit_size / 2 - 100):
+            n = p * q
+            fi = (p - 1) * (q - 1)
+            e = 65537
+            if gcd(e, fi) == 1:
+                break
+
+    
