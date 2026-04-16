5527c97fc8715b7d1f9f783c9cfa4779f666466a
Flyli0 <kirillrozkov653@gmail.com>
2026-04-16 13:23:31 +0500

    same as prev

--- Signatures/Hash_and_sign.py
+++ Signatures/Hash_and_sign.py
@@ -1,9 +1,15 @@
-from EncryptionDecryption.Encryption import encrypt
 from Padding.PKCS_Padding import pkcs_padding
+from helpers.SHA256.SecureHashingAlgorithm import sha_256
 
 
 def sign(message, private_key, bit_size):
-    m = pkcs_padding(message, bit_size)
+    asn1 = bytes.fromhex('3031300d060960864801650304020105000420')
+    h = sha_256(message)
+    digest_info = asn1 + h
+    if len(digest_info) > bit_size//8 - 11:
+        raise ValueError("Message too long")
+
+    m = pkcs_padding(digest_info, bit_size)
     n = private_key[0]
     d = private_key[1]
     s = pow(m,d,n)
---
+++ Signatures/Hash_and_sign_Verification.py
@@ -0,0 +1,29 @@
+from EncryptionDecryption.Decryption import decrypt
+from helpers.SHA256.SecureHashingAlgorithm import sha_256
+
+
+def verify(message, signature, public_key):
+    n = public_key[0]
+    e = public_key[1]
+    m = pow(signature, e, n)
+    k = (n.bit_length() + 7) // 8
+    EM = m.to_bytes(k, "big")
+
+    if EM[0] != 0x00 or EM[1] != 0x01:  # checking structure
+        return False
+
+    try:
+        sep_index = EM.index(b"\x00", 2)
+    except ValueError:
+        return False
+
+    PS = EM[2:sep_index]
+
+    if len(PS) < 8 or any(b != 0xFF for b in PS):
+        return False
+
+    digest_info = EM[sep_index+1:]
+    asn = bytes.fromhex("3031300d060960864801650304020105000420")
+    expected_hash = sha_256(message)
+    expected_info = asn+expected_hash
+    return expected_info == digest_info
